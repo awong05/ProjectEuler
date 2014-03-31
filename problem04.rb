b464d63120f89a81989ff8be7012346a95fbd717
awong05 <Andy.Kee.Wong@gmail.com>
2014-03-31 19:59:04 -0400

    Refactored Problem 4.

--- problem04.rb
+++ problem04.rb
@@ -1,13 +1,9 @@
-def palindrome(n)
-	return n.to_s == n.to_s.reverse
-end
-
 product = 1
 
 for i in 100..999 do
 	for j in 100..999 do
 		prod = j*i
-		product = prod if palindrome(prod) && prod > product
+		product = prod if prod.to_s == prod.to_s.reverse && prod > product
 	end
 end
 
