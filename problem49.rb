efc99cc13f589d6b93e63b97772d944113aed1b9
Andy W <Andy.Kee.Wong@gmail.com>
2015-10-22 20:22:51 -0400

    Reaaally ugly solution. Plz refact0r.

---
+++ problem49.rb
@@ -0,0 +1,13 @@
+require 'prime'
+
+a = Prime.take_while {|n| n < 10_000}.select {|n| n > 1_000}
+b = ''
+
+while a.any?
+	x = a.shift
+	y = x + 3330
+	z = y + 3330
+	b << x.to_s << y.to_s << z.to_s if x != 1487 and y.prime? and z.prime? and x.to_s.chars.sort == y.to_s.chars.sort and y.to_s.chars.sort == z.to_s.chars.sort
+end
+
+puts b
