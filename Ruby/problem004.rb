e18622095815afb4b1dee9850ee1b0bba0d26096
Andy W <Andy.Kee.Wong@gmail.com>
2016-04-03 23:50:06 -0400

    An actual optimization!

--- Ruby/problem004.rb
+++ Ruby/problem004.rb
@@ -1,10 +1,10 @@
 product = 1
 
 for i in 100..999 do
-	for j in 100..999 do
+	for j in i..999 do
 		prod = j*i
 		product = prod if prod.to_s == prod.to_s.reverse && prod > product
 	end
 end
 
-print product
+puts product
