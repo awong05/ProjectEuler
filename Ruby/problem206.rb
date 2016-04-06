cec339dbabbd128ed3ce962bfa3aa3d502cf80d4
Andy W <Andy.Kee.Wong@gmail.com>
2016-04-05 23:57:12 -0400

    Another durhurrr moment.

--- Ruby/problem206.rb
+++ Ruby/problem206.rb
@@ -1,11 +1,17 @@
-(101_010_010...138_902_662).each do |n|
-	next unless n.to_s.chars.last == "3" or n.to_s.chars.last == "7"
+n = 101_010_013
+
+while n < 138_902_657
 	digits = (n ** 2).to_s.split('')
-	next unless digits[0] == "1" and digits[2] == "2" \
+	break if digits[0] == "1" and digits[2] == "2" \
 		and digits[4] == "3" and digits[6] == "4" \
 		and digits[8] == "5" and digits[10] == "6" \
 		and digits[12] == "7" and digits[14] == "8" \
 		and digits[16] == "9"
-	puts n * 10
-	break
+	if n % 10 == 3
+		n += 4
+	elsif n % 10 == 7
+		n += 6
+	end
 end
+
+puts n * 10
