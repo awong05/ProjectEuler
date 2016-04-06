7ce68ffe291a30a6f518c4a8790d504714a05a4a
Andy W <Andy.Kee.Wong@gmail.com>
2016-04-06 00:19:51 -0400

    AHA!

--- Ruby/problem206.rb
+++ Ruby/problem206.rb
@@ -1,17 +1,9 @@
 n = 101_010_013
 
-while n < 138_902_657
-	digits = (n ** 2).to_s.split('')
-	break if digits[0] == "1" and digits[2] == "2" \
-		and digits[4] == "3" and digits[6] == "4" \
-		and digits[8] == "5" and digits[10] == "6" \
-		and digits[12] == "7" and digits[14] == "8" \
-		and digits[16] == "9"
-	if n % 10 == 3
-		n += 4
-	elsif n % 10 == 7
-		n += 6
-	end
+while n <= 138_902_657
+	digits = "#{n ** 2}"
+	break if /1\d2\d3\d4\d5\d6\d7\d8\d9/.match(digits)
+	n += n % 10 == 3 ? 4 : 6
 end
 
 puts n * 10
